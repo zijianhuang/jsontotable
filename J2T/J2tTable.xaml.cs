ab21851ca059b7b70c8ed3feedaffcd8ab598875
Z <zijianhuang2000@gmail.com>
2022-09-21 10:54:06 +1000

    ok

---
+++ J2T/J2tTable.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace J2T
+{
+	/// <summary>
+	/// Interaction logic for J2tTable.xaml
+	/// </summary>
+	public partial class J2tTable : UserControl
+	{
+		public J2tTable()
+		{
+			InitializeComponent();
+			List<User> users = new List<User>();
+			users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+			users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+			users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+			this.nodeSimple.ItemsSource = users;
+		}
+	}
+
+	public class User
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public DateTime Birthday { get; set; }
+	}
+}
